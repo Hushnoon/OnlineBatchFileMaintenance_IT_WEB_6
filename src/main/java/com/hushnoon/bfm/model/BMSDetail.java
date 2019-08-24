ede240b6571b39e8c3f00451a040a8a4ae751219
MRuser <MRuser@KOL-CST-FLM7-R.NIITEDUCATION.COM>
2019-08-24 16:14:48 +0530

    first commit

---
+++ src/main/java/com/hushnoon/bfm/model/BMSDetail.java
@@ -0,0 +1,73 @@
+package com.hushnoon.bfm.model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class BMSDetail {
+
+	private int bmsEntryCode;
+	private Date entryDate;
+	private String observation;
+	private String status;
+	private String action;
+	private Date deadlineDate;
+	private String responsiblePerson;
+	
+	public BatchDetail getBatch() {
+		return batch;
+	}
+	public void setBatch(BatchDetail batch) {
+		this.batch = batch;
+	}
+	@ManyToOne
+    private BatchDetail batch;
+	
+	public int getBmsEntryCode() {
+		return bmsEntryCode;
+	}
+	public void setBmsEntryCode(int bmsEntryCode) {
+		this.bmsEntryCode = bmsEntryCode;
+	}
+	
+	public Date getEntryDate() {
+		return entryDate;
+	}
+	public void setEntryDate(Date entryDate) {
+		this.entryDate = entryDate;
+	}
+	public String getObservation() {
+		return observation;
+	}
+	public void setObservation(String observation) {
+		this.observation = observation;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public Date getDeadlineDate() {
+		return deadlineDate;
+	}
+	public void setDeadlineDate(Date deadlineDate) {
+		this.deadlineDate = deadlineDate;
+	}
+	public String getResponsiblePerson() {
+		return responsiblePerson;
+	}
+	public void setResponsiblePerson(String responsiblePerson) {
+		this.responsiblePerson = responsiblePerson;
+	}
+	
+	
+}
---
+++ src/main/java/com/hushnoon/bfm/model/BatchDetail.java
@@ -0,0 +1,82 @@
+package com.hushnoon.bfm.model;
+
+import java.sql.Date;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CascadeType;
+
+@Entity
+public class BatchDetail {
+	
+	@Id
+	private int batchCode;
+	private String courseCode;
+	private String semesterCode;
+	private String facultyName;
+	private String supervisorName;
+	private Date startDate;
+	private Date endDate;
+	
+	public Set<BMSDetail> getBmsDetails() {
+		return bmsDetails;
+	}
+	public void setBmsDetails(Set<BMSDetail> bmsDetails) {
+		this.bmsDetails = bmsDetails;
+	}
+	@OneToMany
+    @JoinColumn(name="batchCode")
+    private Set<BMSDetail> bmsDetails;
+	
+	
+	public int getBatchCode() {
+		return batchCode;
+	}
+	public void setBatchCode(int batchCode) {
+		this.batchCode = batchCode;
+	}
+	public String getCourseCode() {
+		return courseCode;
+	}
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+	public String getSemesterCode() {
+		return semesterCode;
+	}
+	public void setSemesterCode(String semesterCode) {
+		this.semesterCode = semesterCode;
+	}
+	public String getFacultyName() {
+		return facultyName;
+	}
+	public void setFacultyName(String facultyName) {
+		this.facultyName = facultyName;
+	}
+	public String getSupervisorName() {
+		return supervisorName;
+	}
+	public void setSupervisorName(String supervisorName) {
+		this.supervisorName = supervisorName;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	
+	
+
+}
